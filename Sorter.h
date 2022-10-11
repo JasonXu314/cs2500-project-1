200aaeacc803f618b7ebffc914a96fd21337ec05
Jason Xu <Xinwen31415@gmail.com>
2022-10-11 12:46:19 -0500

    added message about comments

--- Sorter.h
+++ Sorter.h
@@ -8,8 +8,16 @@
 template <class T>
 class Sorter {
 public:
+	// Note: I believe that this sort of comment is inherently HARMFUL to code readability, but since your rubric requires me to write it, here it is
+	// Creates a sorter object
+	// Pre: the > operator is defined for type T
+	// Post: a new Sorter object is created
 	Sorter() {}
 
+	// Note: I believe that this sort of comment is inherently HARMFUL to code readability, but since your rubric requires me to write it, here it is
+	// Sorts the given array arr using insertion sort
+	// Pre: arr is a valid array of size n, the > operator is defined for type T
+	// Post: arr is sorted in ascending order
 	void insertion_sort(T* arr, unsigned int size);
 };
 
